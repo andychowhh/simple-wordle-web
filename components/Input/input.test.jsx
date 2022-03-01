388dc8bf6880d069768775604b0dcf09a2bc051e
Andy Chow <hhchow7-c@my.cityu.edu.hk>
2022-03-01 19:17:20 +0800

    add test case for input for testing multiple input

--- components/Input/input.test.jsx
+++ components/Input/input.test.jsx
@@ -4,7 +4,7 @@ import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 
 describe("Testing Input component", () => {
-  it("renders a Input and show correct value", () => {
+  it("renders a Input and show correct value in one single input box", () => {
     const mockSetValue = jest.fn((x) => {});
     render(
       <Input
@@ -16,4 +16,22 @@ describe("Testing Input component", () => {
     );
     expect(screen.getByTestId("input-0")).toHaveValue("b");
   });
+
+  it("renders a Input and show correct value in all 5 input boxes", () => {
+    const mockSetValue = jest.fn((x) => {});
+    render(
+      <Input
+        value={"abcde"}
+        setValue={(e) => {
+          mockSetValue(e);
+        }}
+      />
+    );
+    expect(screen.getByTestId("input-0")).toHaveValue("a");
+    expect(screen.getByTestId("input-1")).toHaveValue("b");
+    expect(screen.getByTestId("input-2")).toHaveValue("c");
+    expect(screen.getByTestId("input-3")).toHaveValue("d");
+    expect(screen.getByTestId("input-4")).toHaveValue("e");
+  });
+
 });
