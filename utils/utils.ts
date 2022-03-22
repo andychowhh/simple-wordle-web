87350210bf5adb61f9e591d8ce611491a025ad1b
Andy Chow <hhchow7-c@my.cityu.edu.hk>
2022-03-22 12:15:25 +0800

    update the logic of word comparision

--- utils/utils.ts
+++ utils/utils.ts
@@ -6,24 +6,74 @@ export const wordCompare = (
   input: Array<string>,
   target: string
 ): Array<WordCompareResultType> => {
-  let result: Array<WordCompareResultType> = input.map(
-    (letter: string, i: number) => {
-      let targetArr: Array<string> = target.split("");
-      if (targetArr[i] === letter)
-        return {
-          character: letter,
-          result: wordCompareResult.characterMatched,
-        };
-      if (targetArr.includes(letter))
-        return {
-          character: letter,
-          result: wordCompareResult.characterIncluded,
-        };
-      return {
+  let matchedArr: Array<any> = [];
+  let includedArr: Array<any> = [];
+
+  let result: Array<any> = [];
+  // put matched/included char in corresponding arrays
+  input.map((letter: string, i: number) => {
+    let targetArr: Array<string> = target.split("");
+    if (targetArr[i] === letter) {
+      matchedArr.push({
         character: letter,
-        result: wordCompareResult.characterNotIncluded,
-      };
+        index: i,
+      });
+    } else if (targetArr.includes(letter)) {
+      includedArr.push({
+        character: letter,
+        index: i,
+      });
     }
-  );
-  return result;
+    return;
+  });
+  // use targetArr to control the word which is already handled
+  let targetArr: Array<string | null> = target.split("");
+  // if matched, remove the element from the targetArr based on its index, and push it to result[]
+  matchedArr.map((matched) => {
+    let index = matched.index;
+    if (targetArr[index] !== null) {
+      result.push({
+        character: matched.character,
+        result: wordCompareResult.characterMatched,
+        index: matched.index,
+      });
+      targetArr[index] = null;
+    }
+  });
+  // console.log("targetArr", targetArr);
+  // console.log("result", result);
+  // if included, remove the element in targetArr after locating its index
+  includedArr.map((included) => {
+    let characterIndex: number = targetArr.findIndex(
+      (x) => x === included.character
+    );
+    if (characterIndex !== -1) {
+      result.push({
+        character: included.character,
+        result: wordCompareResult.characterIncluded,
+        index: included.index,
+      });
+      targetArr[characterIndex] = null;
+    }
+  });
+  console.log("targetArr", targetArr);
+  console.log("result", result);
+
+  let filteredResult = input.map((ele: string, i: number) => {
+    let obj = {
+      character: ele,
+    };
+    let charResult = result.find((resultItem) => {
+      return resultItem.index === i;
+    });
+    let formattedCharResult = charResult
+      ? charResult.result
+      : wordCompareResult.characterNotIncluded;
+
+    return {
+      ...obj,
+      result: formattedCharResult,
+    };
+  });
+  return filteredResult;
 };
