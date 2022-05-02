767079db66ea09f2bdf894a9ddd0dc9986eed224
Andy Chow <hohin523work@gmail.com>
2022-05-02 12:30:42 +0800

    Add test for showToast

--- containers/ToastContainer/ToastContainer.test.tsx
+++ containers/ToastContainer/ToastContainer.test.tsx
@@ -1,17 +1,35 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
-import ToastContainer from "@/containers/ToastContainer/ToastContainer";
+import ToastContainer, {
+  showToast,
+} from "@/containers/ToastContainer/ToastContainer";
 
 import ComponentMock from "../../__mocks__/componentMock";
 
 describe("ToastContainer", () => {
-  test("should include className toast-container", () => {
-    const { container } = render(
-      <ToastContainer>
-        <ComponentMock />
-      </ToastContainer>
-    );
+  describe("ToastContainer Component", () => {
+    test("should include className toast-container", () => {
+      const { container } = render(
+        <ToastContainer>
+          <ComponentMock />
+        </ToastContainer>
+      );
 
-    expect(container.firstChild).toHaveClass("toast-container");
+      expect(container.firstChild).toHaveClass("toast-container");
+      expect(container.firstChild?.firstChild).toHaveClass("Toastify");
+    });
+  });
+
+  describe("showToast", () => {
+    test("should show toast text in the screen", async () => {
+      render(
+        <ToastContainer>
+          <ComponentMock />
+        </ToastContainer>
+      );
+
+      showToast("Toast Text");
+      expect(await screen.findByText("Toast Text")).toBeInTheDocument();
+    });
   });
 });
